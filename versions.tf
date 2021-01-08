ffd8c81b040f18df04351a20433e3b9f590977aa
Adin Hodovic <hodovicadin@gmail.com>
2021-01-08 15:05:10 +0100

    Change version

--- versions.tf
+++ versions.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
+      version = "> 2.0.0"
     }
   }
   required_version = ">= 0.13"
